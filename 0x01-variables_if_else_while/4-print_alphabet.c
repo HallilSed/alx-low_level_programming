260d882aa915e6875e4eee376119be056bd3f3dc
Hallilsed <hallilsed@gmail.com>
2023-06-16 17:36:49 -0700

    except e and q

---
+++ 0x01-variables_if_else_while/4-print_alphabet.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - Print all letters except q and e
+ *
+ * Return: always 0.
+ */
+int main(void)
+{
+	char lowerq = 'a';
+
+	e = 'e';
+	q = 'q';
+	for (lowerq = 'a'; lowerq <= 'z'; lower++)
+	{
+	if (low != e && low != q)
+	putchar(low);
+	}
+	putchar('\n');
+	return (0);
+}
