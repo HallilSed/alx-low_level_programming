7aa89e2a371a9d70bba1101a4caf56af8bab56dd
Hallilsed <hallilsed@gmail.com>
2023-06-16 18:28:07 -0700

    reverse

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - Print all letters except q and e
+ *
+ * Return: always 0.
+ */
+int main(void)
+{
+	char rev;
+
+	for (rev = 'z'; rev >= 'a'; low++)
+	putchar(rev);
+	putchar('\n');
+	return (0);
+}
