0a8c44de887683a16b3955cd1446dfd93dd89160
Hallilsed <hallilsed@gmail.com>
2023-06-16 18:13:43 -0700

    numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - Print all letters except q and e
+ *
+ * Return: always 0.
+ */
+int main(void)
+{
+	char Num;
+
+	for (Num = 0; Num < 10; Num++)
+	printf("%d", Num);
+	printf("\n");
+	return (0);
+}
