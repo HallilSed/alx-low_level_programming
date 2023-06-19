46ea166d7f9fe329c24b7c7e232d48b8ac68b2c9
Hallilsed <hallilsed@gmail.com>
2023-06-19 10:26:03 -0700

    three digits

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -12,21 +12,21 @@ int main(void)
 
 	for (a = 48; a <= 57; a++)
 	{
-	for (b = a + 49; b <= 57; b++)
-	{
-	for (c = a + b + 50; c <= 57; c++)
-	{
-	if ((a != b) != c)
-	{
-	putchar (a);
-	putchar (b);
-	putchar (c);
-	if (a == 55 && b == 56)
-	putchar (',');
-	putchar (' ');
-	}
-	}
-	}
+		for (b = a + 1; b <= 57; b++)
+		{
+			for (c = b + 1; c <= 57; c++)
+			{
+				putchar(a);
+				putchar(b);
+				putchar(c);
+				if (a == 55 && b == 56 && c == 57)
+				{
+					break;
+				}
+				putchar(',');
+				putchar(' ');
+			}
+		}
 	}
 	putchar ('\n');
 	return (0);
