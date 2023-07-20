126f70d78ed7c060975dc98cf3c88f7bffe228c8
Hallilsed <hallilsed@gmail.com>
2023-07-20 07:57:36 -0700

    performs simple operations

--- 0x0F-function_pointers/3-op_functions.c
+++ 0x0F-function_pointers/3-op_functions.c
@@ -1,6 +1,4 @@
 #include "3-calc.h"
-#include <stdio.h>
-#include <stdlib.h>
 
 /**
  * op_add - adds two integers
@@ -42,11 +40,6 @@ int op_mul(int a, int b)
  */
 int op_div(int a, int b)
 {
-	if (b == 0)
-	{
-		printf("Error\n");
-		exit(100);
-	}
 	return (a / b);
 }
 
@@ -58,10 +51,5 @@ int op_div(int a, int b)
  */
 int op_mod(int a, int b)
 {
-	if (b == 0)
-	{
-		printf("Error\n");
-		exit(100);
-	}
 	return (a % b);
 }
